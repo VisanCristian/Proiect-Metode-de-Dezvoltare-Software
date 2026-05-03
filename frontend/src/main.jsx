850e2be2832d5aebda83905502e935672d804b85
Ioana-caty <caramidaioana28@yahoo.com>
2026-05-03 15:52:52 +0300

    frontend: add axios and update FlashCard entry import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
-import FlashCard from './pages/flash-card/FlashCardApp.jsx'
+import FlashCard from './pages/FlashCard/FlashCardApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
