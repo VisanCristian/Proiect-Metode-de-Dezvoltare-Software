a1381370d2fdb710d6825e3e7af60ffb7e887ba2
Georgiana Spataru <spatarugeorgiana799@gmail.com>
2026-06-15 23:45:30 +0300

    feat: create ActivityReport component shell

---
+++ frontend/src/fragments/Home/ActivityReport.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ActivityReport = () => {
+    return (
+        <div className="activity-report">
+            <h3 className="activity-report__title">Activity</h3>
+        </div>
+    );
+};
+
+export default ActivityReport;
